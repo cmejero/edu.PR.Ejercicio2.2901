ae3127d3f58f6acd1a56fdb60de8cb9356326d39
csi23-cmejero <csi23-cmejero@colegioaltair.net>
2024-02-05 13:27:05 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace edu.PR.Ejercicio2._2901
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+           DateTime fechaActual= DateTime.Now;
+
+            Console.WriteLine("Dia: " + fechaActual.Day);
+            Console.WriteLine("Mes: " + fechaActual.Month);
+            Console.WriteLine("Año: " + fechaActual.Year);
+            Console.WriteLine("Hora: " + fechaActual.Hour);
+            Console.WriteLine("Minutos: " + fechaActual.Minute);
+            Console.WriteLine("Segundos: " + fechaActual.Second);
+            Console.WriteLine("Dia de la semana: " + fechaActual.DayOfWeek);
+            Console.WriteLine("Dia del año: " + fechaActual.DayOfYear);
+           
+
+
+        }
+    }
+}
